025f50bf5c6f8abcf36141ce8787661d7094982c
Erick <edeajob27@gmail.com>
2023-03-08 18:10:47 -0500

    primeras migraciones y modelo Department

--- employee/applications/departments/models.py
+++ employee/applications/departments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField('Nombre', max_length=50)
+    short_name = models.CharField('Alias', max_length=20)
+    anulate = models.BooleanField('Anulate', default=False)
+
+    def __str__(self):
+        return self.name
