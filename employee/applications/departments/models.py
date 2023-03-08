31dd0a202dfb2a8b7cf4b0065abc2633c0e069cc
Erick <edeajob27@gmail.com>
2023-03-08 18:51:35 -0500

    Migraciones y creacion del modelo empleado

--- employee/applications/departments/models.py
+++ employee/applications/departments/models.py
@@ -2,9 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Department(models.Model):
+    """Model for Departments"""
+
+    #Atributos
     name = models.CharField('Nombre', max_length=50)
-    short_name = models.CharField('Alias', max_length=20)
+    short_name = models.CharField('Alias', max_length=20,unique=True)
     anulate = models.BooleanField('Anulate', default=False)
 
+    #Métodos
     def __str__(self):
+        """A method to overwrite __str__() and make the admin panel
+        more human readable
+
+        Returns
+        -------
+        _str_
+            _The name of Department_
+        """
         return self.name
--- employee/applications/employees/models.py
+++ employee/applications/employees/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from applications.departments.models import Department
 
 # Create your models here.
+class Employee(models.Model):
+    """Employees model"""
+    #Choices
+    #! Debe ser una constante (Letras mayusculas)
+    #! Debe ser una lista de tuplas
+    JOB_CHOICES = [
+        ('0','Accountant'),
+        ('1','Manager'),
+        ('2','Economist'),
+        ('3','Other'),
+    ]
+
+    #Atributos
+    first_name = models.CharField("Names", max_length=50)
+    last_name = models.CharField("Last Names", max_length=50)
+    job = models.CharField("Job", max_length=50,choices=JOB_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    #image = models.ImageField('Image', upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    #Métodos
+    def __str__(self):
+        """A method to overwrite __str__() and make the admin panel
+        more human readable
+
+        Returns
+        -------
+        (str)
+            The full name of the employee
+        """
+        return self.first_name + ' ' + self.last_name
