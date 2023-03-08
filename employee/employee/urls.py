305244dc30ed26c631490dfd4278528d704984f8
Erick <edeajob27@gmail.com>
2023-03-07 21:29:52 -0500

    Nuevo comienzo

--- employee/employee/urls.py
+++ employee/employee/urls.py
@@ -1,4 +1,4 @@
-"""Employee URL Configuration
+"""employee URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
