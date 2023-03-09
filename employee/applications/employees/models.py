8b6bbd12b472a79b8a9656556a8f7dfb14e67c64
Erick <edeajob27@gmail.com>
2023-03-08 22:44:36 -0500

    Registrando el modelo Skill en el admin

--- employee/applications/employees/models.py
+++ employee/applications/employees/models.py
@@ -37,7 +37,8 @@ class Employee(models.Model):
     last_name = models.CharField("Last Names", max_length=50)
     job = models.CharField("Job", max_length=50,choices=JOB_CHOICES)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    #image = models.ImageField('Image', upload_to=None, height_field=None, width_field=None, max_length=None)
+    skills = models.ManyToManyField(Skill)
+    avatar = models.ImageField('Avatar-Image', upload_to='avatars', blank=True, null=True)
 
     #Meta
     class Meta:
